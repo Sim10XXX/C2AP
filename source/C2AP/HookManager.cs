384da5278e21794dead9f50fdf8d627feb605087
Sim10XXX <deox.master@gmail.com>
2026-07-25 12:23:21 -0400

    Added clearing of hooks from a previous connection

--- source/C2AP/HookManager.cs
+++ source/C2AP/HookManager.cs
@@ -1,4 +1,5 @@
-﻿using Serilog;
+﻿using Archipelago.Core.Util;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +15,20 @@ namespace C2AP
         private const uint FreeSpaceStart = 0xf000;
 
         private const uint FreeSpaceEnd = 0xfffc;
-
+        
         public static void AddHook(CustomHook hook, uint targetAddress)
         {
+            foreach (CustomHook activeHook in ActiveHooks) 
+            {
+                if (activeHook._targetAddress == targetAddress)
+                {
+                    Log.Error($"A hook is already active at address 0x{targetAddress:X}");
+                    Log.Error("Aborting hook insertion to prevent problems with removing hooks in the wrong order");
+                    return;
+                }
+            }
+
+
             uint freeAddress;
             if (ActiveHooks.Count == 0)
             {
@@ -34,7 +46,6 @@ namespace C2AP
                 Log.Error("Inserted hook exceeded free space limit, game might become unstable");
             }
             ActiveHooks.Add(hook);
-
         }
 
         public static void RemoveHook(CustomHook hook)
@@ -69,7 +80,6 @@ namespace C2AP
                 ActiveHooks[index].InsertHook(targetAddress, freeAddress);
                 freeAddress = (uint)(ActiveHooks[index]._freeAddress + ActiveHooks[index]._hookSize) + 0x4;
             }
-
         }
 
         public static void RefreshHooks()
@@ -90,6 +100,99 @@ namespace C2AP
                 hook.RemoveHook();
             }
             ActiveHooks.Clear();
+        }
+
+        public static void ReplaceAsm(CustomHook hook, List<string> asm)
+        {
+            if (ActiveHooks.All(h => h != hook))
+            {
+                Log.Warning("ReplaceAsm: hook not found");
+                return;
+            }
+            if (hook._targetAddress == 0 && hook._freeAddress == 0)
+            {
+                Log.Warning("can't run ReplaceAsm on uninserted hook");
+                return;
+            }
+            uint targetAddress = (uint) hook._targetAddress;
+            int targetInstructionSize = hook._targetInstructionSize;
+            //ulong freeAddress = hook._freeAddress;
+
+            RemoveHook(hook);
+            hook._asm = asm;
+            hook._bytes = CustomHook.ConvertAsm(asm);
+            //Log.Information("replaceasm1");
+            AddHook(hook, targetAddress);
+            //Log.Information("replaceasm2");
+        }
+
+        public static void ClearHooksFromPreviousConnection()
+        {
+            if (ActiveHooks.Count != 0)
+            {
+                Log.Error("ClearHooksFromPreviousConnection should only be called when HookManager is unaware of any hooks");
+                return;
+            }
+            byte[] bytes = Memory.ReadByteArray(FreeSpaceStart, (int)(FreeSpaceEnd - FreeSpaceStart));
+            int previousZeroes = 0;
+            int hookStartIndex = 0;
+            if (bytes[0] == 0 &&
+                    bytes[1] == 0 &&
+                    bytes[2] == 0 &&
+                    bytes[3] == 0)
+            {
+                //Nothing to clear
+                return;
+            }
+            for (int i = 0; i < bytes.Length; i+=4) 
+            {
+                if (bytes[i] == 0 &&
+                    bytes[i + 1] == 0 &&
+                    bytes[i + 2] == 0 &&
+                    bytes[i + 3] == 0)
+                {
+                    previousZeroes++;
+                    if (previousZeroes >= 3)
+                    {
+                        Log.Information($"Stopping at {i + FreeSpaceStart:X}");
+                        break;
+                    }
+                    if (previousZeroes == 1)
+                    {
+                        continue;
+                    }
+                    // 2 nop's in a row imply the end of a hook, so remove the hook
+
+                    // The first part of the hook containes the original target instructions
+                    byte[] first = bytes[hookStartIndex..(hookStartIndex + 8)];
+
+                    // The jmpback is always the last instruction, and can be used to find the target address
+                    byte[] jmpback = bytes[(i-8)..(i-4)];
+
+                    // Reverse to get rid of little endian (to make it easier for me)
+                    //jmpback.Reverse();
+                    jmpback[3] &= 0x03; // Clear the top 6 bits, since they are not part of the address
+                    uint address = BitConverter.ToUInt32(jmpback.ToArray(), 0);
+                    address -= 2; // Go back 2 instructions, since the jmp goes after the original target instructions
+                    address <<= 2; // Shift left to get the actual address (undoing MIPS encoding)
+
+                    Log.Information($"Target of hook is at {address:X}");
+
+                    // Write the original instructions back to the target address
+                    Memory.WriteByteArray(address, first);
+
+                    // Clear the hook from free space
+                    Memory.WriteByteArray((ulong) hookStartIndex + FreeSpaceStart, new byte[i -hookStartIndex]);
+
+
+                    hookStartIndex = i + 4;
+                }
+                else
+                {
+                    previousZeroes = 0;
+                }
+            }
+
         }
     }
 }
